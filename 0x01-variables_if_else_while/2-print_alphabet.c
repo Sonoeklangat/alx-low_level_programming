09d351bc9fa9067f8b42e1f9bbffdc2f398e6b87
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-17 11:41:30 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: always 0 (successful)
+*/
+
+int main(void)
+
+{
+	char alp;
+
+	for (alp > 'a'; alp <= 'z'; alp++);
+			putchar (alp);
+
+			return (0);
+}
