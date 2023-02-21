50991167a3b2b550d684192640701f6011763bf8
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-21 09:13:27 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign on integer if zero, less that zero or more than zero
+ * @n: initialized integer
+ * Return: 1 if n greater than 0, 0 if n is 0 and -1 if n less than 0
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
