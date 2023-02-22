3bcbed8e6098bd0412438e3b4dfc06889fdee382
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-22 06:17:08 -0800

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints positive or negative number
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
