ddccc7678e0ebd37cb4502c490df4f186a096820
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-22 03:52:08 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,21 +14,33 @@ void print_to_98(int n)
 	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
-			printf("%d, ", i);
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
 		printf("\n");
 	}
 	else if (n < 0)
 	{
 		for (i = n ; i <= 98 ; i++)
-			printf("%d, ", i);
+		{
+			printf("%d", i);
+			if (i != 98)
+                                printf(", ");
+		}
 		printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
-			printf("%d, ", i);
+		{
+			printf("%d", i);
+			if (i != 98)
+                                printf(", ");
+		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
