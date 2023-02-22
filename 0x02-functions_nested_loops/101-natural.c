e723b704db298e442898a0a718b0f1721bda2eec
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-22 04:56:06 -0800

    23

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
-        int sum;
+	int i;
+	int sum;
 
-        sum = 0;
+	sum = 0;
 
-        for (i > 0 ; i < 10 ; i++)
-        {
-                if ((i % 3 == 0) || (i % 5 == 0))
-                {
-                        sum = sum + i;
-                }
-        }
-                printf("%d\n", sum);
-                return (0);
+	for (i > 0 ; i < 10 ; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+	{
+			sum = sum + i;
+	}
+	}
+		printf("%d\n", sum);
+		return (0);
 }
