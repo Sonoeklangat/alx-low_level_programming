18660120c63b79abcadabe384f32fcf226c53261
Sonoeklangat <fabianlangat400@gmail.com>
2023-02-23 04:16:09 -0800

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiply two integers
+ *@a: variable
+ *@b: variable
+ *Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = (a * b);
+	return (mul);
+}
